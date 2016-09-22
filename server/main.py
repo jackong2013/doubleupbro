09252038a20749d2d18a56889e026644cab4fbbb
Jack Ong <jackong2013@gmail.com>
2016-09-22 14:27:29 +0800

    no message

--- server/main.py
+++ server/main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/second_page")
+def second_page():
+	return "you are at second page"
+
 if __name__ == "__main__":
     app.run()
